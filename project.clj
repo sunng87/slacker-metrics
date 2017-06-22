371abfe4a04d1e7c0e82b1a04e87eb1c6edd2fbe
Ning Sun <sunng@about.me>
2017-06-22 23:08:58 +0800

    (chore) use slacker namespace
    Signed-off-by: Ning Sun <sunng@about.me>

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject slacker-metrics "0.1.0-SNAPSHOT"
+(defproject slacker/slacker-metrics "0.1.0-SNAPSHOT"
   :description "A metrics extension for slacker rpc"
   :url "http://github.com/sunng87/slacker-metrics"
   :license {:name "Eclipse Public License"
