bd983fb26a35e99d6aef59b324f239262441640d
Ning Sun <sunng@about.me>
2017-07-22 18:27:21 +0800

    (feat) update dependencies
    Signed-off-by: Ning Sun <sunng@about.me>

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[metrics-clojure "2.9.0"]
-                 [slacker "0.15.0-SNAPSHOT"]]
+                 [slacker "0.15.0"]]
   :profiles {:example {:source-paths ["examples"]}
              :dev {:dependencies [[org.clojure/clojure "1.8.0"]]}}
   :deploy-repositories {"releases" :clojars}
