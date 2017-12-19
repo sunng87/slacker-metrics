502c98d84bbbe7ba0cd4c57d8b68c3811e72952b
Ning Sun <sunng@about.me>
2017-12-20 01:06:56 +0800

    (feat) deps up, remove unused test
    Signed-off-by: Ning Sun <sunng@about.me>

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "http://github.com/sunng87/slacker-metrics"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[metrics-clojure "2.9.0"]
-                 [slacker "0.15.0"]]
+  :dependencies [[metrics-clojure "2.10.0"]
+                 [slacker "0.16.0"]]
   :profiles {:example {:source-paths ["examples"]}
-             :dev {:dependencies [[org.clojure/clojure "1.8.0"]]}}
+             :dev {:dependencies [[org.clojure/clojure "1.9.0"]]}}
   :deploy-repositories {"releases" :clojars}
   :aliases {"run-example" ["trampoline" "with-profile" "default,example" "run" "-m" "slacker-metrics.example.main"]})
